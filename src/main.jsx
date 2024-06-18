5896c5ce3cfd420eb52c91e1867308b4d81ce299
iago-guinossi <108906641+iago-guinossi@users.noreply.github.com>
2024-06-17 21:08:22 -0300

    create global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { ThemeProvider } from 'styled-components'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global.js";
 
-import theme from './styles/theme.js'
+import theme from "./styles/theme.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <App />
+      <GlobalStyles/>
+      <App />
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
