afc35995d723b95af453793f53ed3d149b7534ff
iago-guinossi <108906641+iago-guinossi@users.noreply.github.com>
2024-06-19 20:46:43 -0300

    feat: create buttonText

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+    background-color: none;
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    border: none;
+    font-family: "Poppins", sans-serif;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 24px;
+`
