7d709852555ec4c7b91e370a12b66cbb1c9d0494
iago-guinossi <108906641+iago-guinossi@users.noreply.github.com>
2024-06-23 15:22:58 -0300

    fat: add handleFunction on click

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -2,14 +2,14 @@ import { Contentcontainer, HeaderContainer } from "./stles";
 
 import {Input} from "../Input"
 
-import MenuIcon from "../../assets/menu.svg?react"
+import CloseIcon from "../../assets/close.svg?react"
 import Magnifier from "../../assets/magnifier.svg?react"
 
-export function Menu(){
+export function Menu({handleFuntion}){
     return(
         <>
         <HeaderContainer>
-            <button><MenuIcon/></button>
+            <button onClick={() => handleFuntion()}><CloseIcon/></button>
             Menu
         </HeaderContainer>
         <Contentcontainer>
