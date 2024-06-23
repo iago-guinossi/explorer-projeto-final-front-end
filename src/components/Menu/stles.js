bc8e8e2eaadafd1d7184d284b6f5044f8706e27f
iago-guinossi <108906641+iago-guinossi@users.noreply.github.com>
2024-06-23 15:22:36 -0300

    fix: padding and font family

--- src/components/Menu/stles.js
+++ src/components/Menu/stles.js
@@ -5,7 +5,7 @@ export const HeaderContainer = styled.div`
     height: 114px;
     color: ${({theme}) => theme.COLORS.LIGHT_100};
     background-color: ${({theme}) => theme.COLORS.DARK_700};
-    padding: 56px 0 0 28px;
+    padding: 56px 28px 24px;
     display: flex;
     align-items: center;
     font-family: "Roboto", sans-serif;
@@ -23,12 +23,12 @@ export const HeaderContainer = styled.div`
 
 export const Contentcontainer = styled.div`
     width:100vw;
-    height: 100%;
+    min-height: calc( 100vh - 191px);
     padding: 36px 28px 14px;
     >button {
         width:100%;
         text-align: start;
-        font-family: "Roboto", sans-serif;
+        font-family: "Poppins", sans-serif;
         font-weight: 300;
         font-size: 24px;
         line-height: 140%;
